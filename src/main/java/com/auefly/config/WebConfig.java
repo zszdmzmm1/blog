40529a8bcbefa08dd95f08e42e684178582db484
zszdmzmm <1075534380@qq.com>
2023-10-15 05:11:34 +0800

    WebMvcConfigurer:

--- src/main/java/com/auefly/config/WebConfig.java
+++ src/main/java/com/auefly/config/WebConfig.java
@@ -2,8 +2,20 @@ package com.auefly.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("com.auefly.controller")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp();
+    }
 }
