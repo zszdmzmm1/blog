f30a973fc33dedb9fe32b7bca5877b8c8ef3245d
zszdmzmm <1075534380@qq.com>
2023-10-15 01:13:58 +0800

    前端 prepare

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,6 @@
+import '../scss/app.scss'
+import $ from 'jquery'
+window.jQuery = window.$ = $
+import * as Popper from "bootstrap"
+import 'admin-lte/dist/js/adminlte.min.js'
+import * as tocbot from 'tocbot'
